a74814eea4c9eaafa00edb2fd741c02d074ed883
vigilanteX <dixitaman8@gmail.com>
2025-11-09 13:15:12 +0530

    intro to let variable

---
+++ Section2/variables.js
@@ -0,0 +1,6 @@
+
+//let was introduced in es6, and we cannot use before declaring variable and error-ReferenceError: Cannot access 'c' before initialization
+// let a='a'
+// let b=2
+// console.log(a,b,c)
+// let c='aman'
