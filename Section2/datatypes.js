1a0e07245422496fb40cf02d2ce7c58aa8294f73
vigilanteX <dixitaman8@gmail.com>
2025-11-09 13:52:12 +0530

    datatype section initialized. intro to primitive using typeof to get value type

---
+++ Section2/datatypes.js
@@ -0,0 +1,12 @@
+// 1-String
+// 2-Number
+// 3-Boolean
+// 4-Null-intentionally assigned
+// 5-Undefined-when you just declare n don't assign value
+// 6-Symbol
+// 7-BigInt-value which number can't handle
+//
+// Note JS IS DYANMIC TYPE MEANS DATATYPE IS NOT DEFINED
+
+const s="aman"
+console.log(typeof s)
