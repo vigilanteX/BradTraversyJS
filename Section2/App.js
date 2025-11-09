5b62b6635098b6e8e916c814099507358e6db898
vigilanteX <dixitaman8@gmail.com>
2025-11-09 12:48:30 +0530

    different methods in console object: group using css style in console.log

--- Section2/App.js
+++ Section2/App.js
@@ -1,8 +1,15 @@
 console.log(100)
 console.log(100,'aman','dixit')
 console.log(100,'aman',true)
-
-
 console.warn("hello warn")
 console.error("hello error")
 
+console.group('master group')
+console.log('message from master group')
+console.warn('warn from master group')
+console.error('error from master group')
+console.groupEnd()
+
+
+const style='color:red;background:black'
+console.log('%c styles',style)
