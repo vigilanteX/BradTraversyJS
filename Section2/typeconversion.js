fdcd0345057c99e2131086f10cdffe83fa9addf9
vigilanteX <dixitaman8@gmail.com>
2025-11-10 20:06:37 +0530

    changing string to number

---
+++ Section2/typeconversion.js
@@ -0,0 +1,15 @@
+let amount='100'
+console.log(typeof amount)
+
+//way1 to convert string to number
+// amount=parseInt(amount)
+// console.log(typeof amount)
+
+//way2 to convert string to number
+// amount= +amount
+// console.log(typeof amount)
+
+
+//way3 to convert string to number
+// amount=Number(amount)
+// console.log(typeof amount)
