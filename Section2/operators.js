a1e540646ecd73799c3aa864e2c1bef14c478b11
vigilanteX <dixitaman8@gmail.com>
2025-11-12 22:09:54 +0530

    operators added

---
+++ Section2/operators.js
@@ -0,0 +1,28 @@
+//1-arithmetic
+
+let x;
+x=1+1;
+x=1-1;
+x=1*4
+x=4/2
+x=4%2
+
+//concatenate
+x='Hello '+'world'
+
+//power
+x=2**2
+
+//increment
+x=1;
+x=x+1;
+x+=5
+
+//comparison
+x=2==2
+
+x=5
+
+x=x!=='5'
+console.log(x);
+
