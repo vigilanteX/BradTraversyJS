f2982a59399426e2ef928ff3beb30f66a764add2
vigilanteX <dixitaman8@gmail.com>
2025-11-13 22:18:12 +0530

    Math object

---
+++ Section2/MathObject.js
@@ -0,0 +1,10 @@
+console.log(Math.sqrt(9))
+console.log(Math.abs(-500))
+console.log(Math.round(4.5)) //value>=0.5 will be next else floor
+
+
+console.log(Math.ceil(3.2))
+console.log(Math.floor(3.2))
+console.log(Math.pow(2,4))
+console.log(Math.max(1,2))
+console.log(Math.random())
