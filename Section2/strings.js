c9153dbdeb9425f94f0a7c779a95ceb611ca296c
vigilanteX <dixitaman8@gmail.com>
2025-11-13 18:34:54 +0530

    strings

--- Section2/strings.js
+++ Section2/strings.js
@@ -6,4 +6,29 @@ const age=24
 x='My name is '+name+' '+age
 
 x=`Hello this is my literal with name ${name} and age ${age} and height is ${180+1} cm`
-console.log(x)
+
+const c='hello'
+x=c.length
+
+x=new String('aman')
+x=x.toUpperCase()
+console.log(x.slice(-5,-1))
+console.log(x.substring(0,3))
+console.log(x.charAt(0))
+console.log(x[1])
+console.log(x.indexOf('M'))
+console.log(x, typeof x)
+
+const z='    HELHO        '
+console.log(z.trim())
+console.log(z.trim().replace('H','X'))
+console.log(z.trim().replaceAll('H','X'))
+
+const d='HELAMANMXC'
+console.log(d.includes('AMAN'))
+
+const e=new String("jan")
+console.log(e.valueOf())
+
+const f='hello aman dixit'
+console.log(f.split(' '))
