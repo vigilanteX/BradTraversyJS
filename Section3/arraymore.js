cf14e97b0cd28dcaf1ae027550cb8503d4ebd007
vigilanteX <dixitaman8@gmail.com>
2025-11-14 12:04:09 +0530

    concat arrays, spread operator

---
+++ Section3/arraymore.js
@@ -0,0 +1,15 @@
+let fruits=['banana','berries']
+let fruits2=['grapes','strawberry']
+// fruits.push(fruits2)
+// console.log(fruits)//by this fruits last index will be pushed with an array
+// //[ 'banana', 'berries', [ 'grapes', 'strawberry' ] ]
+// console.log(fruits[2])
+// //[ 'grapes', 'strawberry' ]
+
+// let master=fruits.concat(fruits2)
+// console.log(master)
+// //[ 'banana', 'berries', 'grapes', 'strawberry' ]
+
+//spread
+let x=[...fruits,...fruits2]
+console.log(x)
