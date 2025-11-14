46284e1682acef3a7146d1b0fea4601a4a3bc16c
vigilanteX <dixitaman8@gmail.com>
2025-11-14 11:17:43 +0530

    array important allows entering new element at endindex+1. Example array has 3 elements add arr[3]=value

---
+++ Section3/arrays.js
@@ -0,0 +1,11 @@
+//array literal
+let x=[1,2,3,4,5,6]
+console.log(x)
+
+//array ctor
+let y=new Array(1,2,3,4,5,6,7)
+console.log(y)
+
+let fruits=['apple','peach']
+fruits[2]='grapes'
+console.log(fruits)
