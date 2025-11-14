4f1fed1ff4d0bf093b3ecc4ee83e56db8a653f15
vigilanteX <dixitaman8@gmail.com>
2025-11-14 11:45:08 +0530

    array methods

---
+++ Section3/arraymethods.js
@@ -0,0 +1,21 @@
+const arr=[1,2,3,55,3,2,5,7,8,9,2,33]
+arr.push(12)
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+// arr.unshift(1000)
+// console.log(arr)
+// arr.shift(1000)
+// console.log(arr)
+// arr.reverse()
+// console.log(arr)
+// console.log(arr.includes(2))
+// console.log(arr.indexOf(2))
+// console.log(arr.indexOf(289))//if not found return -1
+// console.log(arr.slice(0,2))//dont modify original array
+arr.splice(0,4)//modify original array from start index value(including) start deleting x
+console.log(arr)
+
+const x=[1,2,3,4,5]
+console.log(x.toString())//converts array to string
+
