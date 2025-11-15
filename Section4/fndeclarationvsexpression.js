30c359921d1345e9e1e15fa4b7753d7da9ba3f4a
vigilanteX <dixitaman8@gmail.com>
2025-11-15 22:52:29 +0530

    fn hoisting

--- Section4/fndeclarationvsexpression.js
+++ Section4/fndeclarationvsexpression.js
@@ -1,11 +1,11 @@
-//function declaration
+//function declaration-hoisting is applicable
 function adddollar()
 {
     console.log('fn dec')
 }
 adddollar();
 
-//function expression
+//function expression-hoisting is not applicable
 const x=function c()
 {
     console.log('fn exp')
