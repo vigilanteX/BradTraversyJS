82fa6757d34b001766d6900943363f13fcbe2490
vigilanteX <dixitaman8@gmail.com>
2025-11-15 23:21:26 +0530

    IIFE-IIFE_Imediately_Invoked_Function_Expression

---
+++ Section4/IIFE_Imediately_Invoked_Function_Expression/script.js
@@ -0,0 +1,13 @@
+//Uncaught SyntaxError: Identifier 'user' has already been declared
+(function () {
+    const user = 'adarsh'
+    console.log(user);
+})();
+
+
+(function (x) {
+    console.log(x);
+})('aman');
+
+
+
