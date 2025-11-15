fc6b28fd05cba0b1757938a4f86cd2fdadf7ca6a
vigilanteX <dixitaman8@gmail.com>
2025-11-15 20:03:40 +0530

    VAR CASES

---
+++ Section4/SCOPES/morescopes.js
@@ -0,0 +1,38 @@
+// const x=12;
+// if (true)
+// {
+//     const y=200
+//     console.log(x+y)//outer x + inner y
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+//console.log(i)//ReferenceError: i is not defined
+
+
+//VAR vs CONST vs LET
+if (true)
+{
+    let f=11
+    const g = 12
+    var h = 13
+}
+//console.log(f)//ReferenceError: f is not defined-block scoped
+//console.log(g)//ReferenceError: g is not defined-blocked scoped
+//console.log(h) worked as h is var and var is global scoped
+
+//let and var in loops
+// for (let i = 0; i < 10; i++) {
+//
+// }
+// //console.log(i)//ReferenceError: i is not defined
+// for (var j = 0; j <=10; j++) {
+//
+// }
+// console.log(j)//works as var is non blocked 11
+
+
+
+
+
--- Section4/SCOPES/script.js
+++ Section4/SCOPES/script.js
@@ -1,6 +1,18 @@
-console.log(window)// global object containing all methods
-alert('meow')//works as this method alert is in global scope
+//console.log(window)// global object containing all methods
+//alert('meow')//works as this method alert is in global scope
 //alert is inside window object
 
-//ANYTHING NOT INSIDE ANY BLOCK OR FUNCTION IS GLOBAL
+//ANYTHING NOT INSIDE ANY BLOCK OR FUNCTION IS GLOBAL SCOPE
 
+
+//function scope:var declared inside fn cannot be accessible outside
+const a=44
+const x=1000;
+function print()
+{
+    const x=12
+    console.log(x)//always take inner block scope priority-var shadowing
+    console.log(a)
+
+}
+print()
