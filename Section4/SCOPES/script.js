ff61cdf4dc7e8068d595391ca6b4c60ad05afbc9
vigilanteX <dixitaman8@gmail.com>
2025-11-15 19:47:19 +0530

    GLOBAL SCOPE

---
+++ Section4/SCOPES/script.js
@@ -0,0 +1,6 @@
+console.log(window)// global object containing all methods
+alert('meow')//works as this method alert is in global scope
+//alert is inside window object
+
+//ANYTHING NOT INSIDE ANY BLOCK OR FUNCTION IS GLOBAL
+
