a26722c33b5fee5e24ef7b4e10b060f636b97656
vigilanteX <dixitaman8@gmail.com>
2025-11-15 13:35:08 +0530

    JSON.parse() convertsJSON to js object

--- Section3/somejson.js
+++ Section3/somejson.js
@@ -6,3 +6,9 @@ let r={
 //JSON.stringify() converts js object to JSON
 let result=JSON.stringify(r)
 console.log(result)
+//NOTE WE CANNOT ACCESS JSON property by . annotation.
+//TO ACCESS you first need to parse
+
+//JSON.parse() converts JSON to object literal
+let result2=JSON.parse(result)
+console.log(result2)
