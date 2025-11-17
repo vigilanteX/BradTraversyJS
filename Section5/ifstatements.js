8c3b99af3ebf81636723178e76978d0d42b48b43
vigilanteX <dixitaman8@gmail.com>
2025-11-17 18:57:18 +0530

    var is block scoped-if (true)                     {                         let a=12;                         const b=13;                         var c=14;                     }                     console.log(c)                     console.log(b)                     console.log(a)

---
+++ Section5/ifstatements.js
@@ -0,0 +1,19 @@
+if (true)
+{
+    console.log('this is 1st true')
+}
+
+if (true)
+{
+    console.log('this is 2nd true')
+}
+
+if (true)
+{
+    let a=12;
+    const b=13;
+    var c=14;
+}
+console.log(c)
+console.log(b)
+console.log(a)
