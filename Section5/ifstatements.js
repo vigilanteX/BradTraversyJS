cb2012d663917192dbb04b4441bed5c835d14dc5
vigilanteX <dixitaman8@gmail.com>
2025-11-17 19:52:15 +0530

    switches

--- Section5/ifstatements.js
+++ Section5/ifstatements.js
@@ -27,3 +27,39 @@ if (l>m)console.log('l>m')
 else if (l==m)console.log('l==m'),
     console.log('lem2')
 else console.log('lem3')
+
+
+//switch
+//case 1 normal exact value compare
+let age=30;
+switch (age)
+{
+    case 10:
+        console.log('10')
+        break;
+    case 20:
+        console.log('20')
+        break;
+    case 30:
+        console.log('30')
+        break;
+    default:
+        console.log('default')
+}
+
+//case2 condisitons in cases
+switch (true)
+{
+    case age<18:
+        console.log('18')
+        break;
+    case age<=30:
+        console.log('30')
+        break;
+    case age>=40:
+        console.log('40')
+        break;
+    default:
+        console.log('default switch 2')
+
+}
