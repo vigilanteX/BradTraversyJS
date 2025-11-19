5557f93dc73b9265b2cebb3f4a74907fbddd8b07
vigilanteX <dixitaman8@gmail.com>
2025-11-19 19:07:25 +0530

    For in loops-return key in objects and index in arrays

---
+++ Section6/LoopForIn.js
@@ -0,0 +1,18 @@
+// let obj={
+//     color1:'red',
+//     color2:'green',
+//     color3:'blue',
+//     color4:'yellow',
+//     color5:'black',
+//     color6:'pink',
+// }
+// for (let key in obj)
+// {
+//     console.log(key,obj[key]);
+// }
+
+let arr=[1,2,3,4,5,6,7]
+for (let key in arr)
+{
+    console.log(key,arr[key])
+}
