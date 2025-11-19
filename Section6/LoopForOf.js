6d59d6972f8f90269850f8cdef36e66821d51256
vigilanteX <dixitaman8@gmail.com>
2025-11-19 18:59:21 +0530

    For of loops for iterating-array, string and map

---
+++ Section6/LoopForOf.js
@@ -0,0 +1,23 @@
+//array iteration
+//let x=[1,2,3,4,5,6,7,8]
+// for (let element of x)
+// {
+//     console.log(element)
+// }
+
+//string iteration
+// let str='aman'
+// for (let element of str)
+// {
+//     console.log(element)
+// }
+
+//map iteration
+// let x=new Map()
+// x.set("x",1)
+// x.set("y",2)
+// x.set("z",3)
+// for(let [key,val] of x )
+// {
+//     console.log(`${key} has value ${val}`)
+// }
