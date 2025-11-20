f439eec402578dd546a9b9e67971aa5085c70637
vigilanteX <dixitaman8@gmail.com>
2025-11-20 19:46:46 +0530

    Reduce

--- Section6/ReduceMethod.js
+++ Section6/ReduceMethod.js
@@ -1,8 +1,8 @@
-let arr=[1,2,3,4,5,6]
-let red=arr.reduce((prev,curr)=>{
-    return prev+curr
-},0)
-console.log(red)
+// let arr=[1,2,3,4,5,6]
+// let red=arr.reduce((prev,curr)=>{
+//     return prev+curr
+// },0)
+// console.log(red)
 
 // //itteration goes
 // IT 1: initial is 0 so prev is 0 and 1(prev+curr)=1
@@ -11,3 +11,15 @@ console.log(red)
 // IT 4: prev is 6 and 4(prev+curr)=10
 // IT 5: prev is 10 and 5(prev+curr)=15
 // IT 6: prev is 15 and 6(prev+curr)=21
+
+
+const cart = [
+    { id: 1, name: 'Product 1', price: 130 },
+    { id: 2, name: 'Product 2', price: 150 },
+    { id: 3, name: 'Product 3', price: 200 },
+];
+let result=cart.reduce(function(prev,curr)
+{
+return prev + curr.price;
+},0)
+console.log(result);
