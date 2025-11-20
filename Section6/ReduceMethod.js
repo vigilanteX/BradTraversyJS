4406605f3d384f2ab8577f1d9c940bcd77d9d1cb
vigilanteX <dixitaman8@gmail.com>
2025-11-20 19:35:47 +0530

    Reduce

---
+++ Section6/ReduceMethod.js
@@ -0,0 +1,13 @@
+let arr=[1,2,3,4,5,6]
+let red=arr.reduce((prev,curr)=>{
+    return prev+curr
+},0)
+console.log(red)
+
+// //itteration goes
+// IT 1: initial is 0 so prev is 0 and 1(prev+curr)=1
+// IT 2: prev is 1 and 2(prev+curr)=3
+// IT 3: prev is 3 and 3(prev+curr)=6
+// IT 4: prev is 6 and 4(prev+curr)=10
+// IT 5: prev is 10 and 5(prev+curr)=15
+// IT 6: prev is 15 and 6(prev+curr)=21
