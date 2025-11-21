f54851130ba8af64b7b09319850a3624fb560cf9
vigilanteX <dixitaman8@gmail.com>
2025-11-21 19:06:28 +0530

    DOM INTRO

---
+++ Section7/DOMINTRO.js
@@ -0,0 +1,27 @@
+// console.dir(window.document)
+// console.log(window.document.body)
+// console.log(window.document.body.innerText)
+// console.log(window.document.body.innerHTML)
+// console.log(window.document.links[0],window.document.links[1],
+//     window.document.links[2]
+// )
+//
+
+//Replace whole HTML content of body with AMAN with h1 tag
+//window.document.body.innerHTML='<h1>AMAN</h1>'
+
+//append at end
+//document.write('Hello World!');
+
+//get by id element and get all text inside
+//console.log(document.getElementById('mellow').innerText)
+
+
+//get by id element and get inner html inside
+//console.log(document.getElementById('nbvc').innerHTML)
+
+//search by id and replace inner text
+let searchedElement=document.getElementById('nbvc')
+//searchedElement.innerHTML='<h2>this is h2 element</h2>'
+//searchedElement.innerText='this is h2 element'
+
