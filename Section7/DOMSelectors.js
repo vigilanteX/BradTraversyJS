05ee9a9d13f7e2a2a3a3c5523b40e8552bbd1f4a
vigilanteX <dixitaman8@gmail.com>
2025-11-21 20:42:29 +0530

    document.querySelector()

---
+++ Section7/DOMSelectors.js
@@ -0,0 +1,17 @@
+let x=document.getElementById('head-title')
+//x=x.className
+//x=x.getAttribute('class')
+// x=x.setAttribute('class','class4 class5')
+// console.log(x)
+
+
+x.textContent='AMAN'
+x.innerText='AMAN'
+x.innerHTML=`<h6>AMAN</h6>`
+x.style.backgroundColor='blue'
+
+let y=document.querySelector('li')
+y.style.color='blue'
+console.log(y)
+
+
