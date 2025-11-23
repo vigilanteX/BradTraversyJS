dbf92c20d8e4817c34fea4911292b56833cbe90b
vigilanteX <dixitaman8@gmail.com>
2025-11-23 12:33:59 +0530

    replaceAll

--- Section7/replaceElementMethod.js
+++ Section7/replaceElementMethod.js
@@ -1,6 +1,19 @@
-const h1id=document.querySelector('#h1id')
-const newH1=document.createElement('h1')
-newH1.setAttribute('id','new-div')
-newH1.innerText='Hello World to be replaced!'
+// const h1id=document.querySelector('#h1id')
+// const newH1=document.createElement('h1')
+// newH1.setAttribute('id','new-div')
+// newH1.innerText='Hello World to be replaced!'
 
-h1id.replaceWith(newH1)
+//h1id.replaceWith(newH1)
+
+
+function replaceAll()
+{
+    const lis=document.querySelectorAll('li');
+    lis.forEach(function(item)
+    {
+        item.innerHTML='Edited'
+
+    })
+
+}
+replaceAll();
