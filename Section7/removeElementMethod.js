2a585c27676629280f2e4bf79f46f9c3063eec28
vigilanteX <dixitaman8@gmail.com>
2025-11-23 13:22:59 +0530

    remove child

--- Section7/removeElementMethod.js
+++ Section7/removeElementMethod.js
@@ -1,2 +1,9 @@
-const clearId=document.querySelector('#clear')
-clearId.remove()
+// //REMOVE
+// const clearId=document.querySelector('#clear')
+// clearId.remove()
+
+//REMOVE CHILD
+// const ul=document.querySelector('ul');
+// const li = ul.querySelector('li:nth-child(2)');
+// ul.removeChild(li)
+// console.log(li.textContent)
