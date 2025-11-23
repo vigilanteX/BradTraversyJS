3933eafb51fb23bb89a348eb34a3a68357c6b059
vigilanteX <dixitaman8@gmail.com>
2025-11-23 13:51:03 +0530

    className and styles

---
+++ Section7/stylesAndClasses.js
@@ -0,0 +1,5 @@
+const stylesOfUL=document.querySelector('ul')
+stylesOfUL.classList.add('meow')
+stylesOfUL.style.backgroundColor= 'red'
+console.log(stylesOfUL.classList)
+console.log(stylesOfUL.className)
