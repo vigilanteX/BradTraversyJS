437f16a02684c1771b9d565030572b999918da58
vigilanteX <dixitaman8@gmail.com>
2025-11-23 12:50:13 +0530

    replace child- called on parent

--- Section7/replaceElementMethod.js
+++ Section7/replaceElementMethod.js
@@ -6,14 +6,26 @@
 //h1id.replaceWith(newH1)
 
 
-function replaceAll()
+// function replaceAll() {
+//     const lis = document.querySelectorAll('li');
+//     lis.forEach(function (item) {
+//         item.innerHTML = 'Edited'
+//
+//     })
+//
+// }
+//
+// replaceAll();
+
+
+function replaceH1Header()
 {
-    const lis=document.querySelectorAll('li');
-    lis.forEach(function(item)
-    {
-        item.innerHTML='Edited'
-
-    })
-
+    const header=document.querySelector('#header')
+    console.log(header)
+    const h1=document.querySelector('#meow')
+    const h4=document.createElement('h4')
+    h4.setAttribute('id','new value')
+    h4.innerText='h2 new World'
+    header.replaceChild(h4,h1)
 }
-replaceAll();
+replaceH1Header()
