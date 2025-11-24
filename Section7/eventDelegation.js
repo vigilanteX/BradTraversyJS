27f693d4bf72a5884086622b8f82354ab6bfcda0
vigilanteX <dixitaman8@gmail.com>
2025-11-24 20:24:14 +0530

    event delegation, window object and page loading

---
+++ Section7/eventDelegation.js
@@ -0,0 +1,12 @@
+const lis=document.querySelectorAll('li')
+// lis.forEach((e)=>{
+//     e.addEventListener('click',f=>{
+//         f.target.remove()
+//     })
+// })
+
+// const ul=document.querySelector('ul')
+// ul.addEventListener('click', e=>{
+//     console.log(e.target.tagName==='LI')
+//     console.log(e.target)
+// })
---
+++ Section7/pageLoading.js
@@ -0,0 +1,19 @@
+// window.onload = function () {
+//     const x = document.querySelector('h1');
+//     console.log(x.classList)
+// }
+//DOM LOADING('DOMContentLoaded')>>>>>>>FASTER>>>>>>>> PAGELOAD('load')
+// window.addEventListener('load',()=>{
+//     console.log("PAGE LOADED");
+// })
+//
+// window.addEventListener('DOMContentLoaded',()=>{
+//     console.log("DOM LOADED");
+// })
+// console.log('meow script loaded');
+
+//<script src="pageLoading.js" defer></script>: defer keyword is same like
+//DOMContentLoaded
+
+// const x = document.querySelector('h1');
+// x.textContent = 'Loading...';
