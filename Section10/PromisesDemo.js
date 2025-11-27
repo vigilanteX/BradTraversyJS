78174af4f2ad48ba7768df42332135293862cc26
vigilanteX <dixitaman8@gmail.com>
2025-11-27 16:25:27 +0530

    handling promise error case

--- Section10/PromisesDemo.js
+++ Section10/PromisesDemo.js
@@ -1,24 +1,36 @@
-// let x=36;
-// let p=new Promise((resolve,reject)=>{
+//Creating promise
+// let x = 36;
+// let p = new Promise((resolve, reject) => {
 //
-//     if (x%2===0)
-//         resolve({'data':{'name':'Aman','age':26}});
+//     if (x % 2 === 0)
+//         resolve({'data': {'name': 'Aman', 'age': 26}});
 //     else
 //         reject('rejected');
 // })
 //
-// p.then(x=>console.log(`value is ${x.data.name}`)).catch(x=>console.log(`error: ${x}`));
+// p.then(x => console.log(`value is ${x.data.name}`)).catch(x => console.log(`error: ${x}`));
 
 
-const p2=new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        console.log('resolved')
-        resolve({'name':'Aman'})
-    },8000)
-})
-p2.then((x)=>console.log(x))
+//resolving promise after 8 sec and replicating API response.
+// const p2=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         console.log('resolved')
+//         resolve({'name':'Aman'})
+//     },8000)
+// })
+// p2.then((x)=>console.log(x))
+// console.log('non blocking code line 1')
+// console.log('non blocking code line 2')
+// console.log('non blocking code line 3')
 
-console.log('non blocking code line 1')
-console.log('non blocking code line 2')
-console.log('non blocking code line 3')
 
+//handling promise error using catch block
+// let p3 = new Promise((resolve, reject) => {
+//     let n = 3
+//     n % 2 === 0 ? resolve('promise resolved') : reject('promise rejected');
+// })
+// p3.then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log(err)
+// })
