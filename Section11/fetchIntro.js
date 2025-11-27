8f81fbc61d454b9ca9166dae9c04cb59d778ef20
vigilanteX <dixitaman8@gmail.com>
2025-11-27 17:30:08 +0530

    Read text file using fetch API

--- Section11/fetchIntro.js
+++ Section11/fetchIntro.js
@@ -4,6 +4,11 @@
 // .then(data=>console.log(data))
 
 //FILE READER
-fetch('./res.json')
-.then(res => res.json())
-.then(data => console.log(data))
+// fetch('./res.json')
+// .then(res => res.json())
+// .then(data => console.log(data))
+
+//READ TEXT FILE
+// fetch('./demo.txt').
+//     then(res=>res.text())
+//     .then(text => console.log(text))
