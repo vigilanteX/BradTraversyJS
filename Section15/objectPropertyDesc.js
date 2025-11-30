030681b7b1115408c3c60470f9924ad914105e96
vigilanteX <dixitaman8@gmail.com>
2025-11-30 22:51:27 +0530

    flag to disable looping on certain prop

--- Section15/objectPropertyDesc.js
+++ Section15/objectPropertyDesc.js
@@ -4,11 +4,22 @@ const reactObj =
         age: 12222,
         number: 9876543210
     }
-    reactObj.name='cvbnmxz'
-console.log(Object.getOwnPropertyDescriptor(reactObj,'name'));
+//     reactObj.name='cvbnmxz'
+// console.log(Object.getOwnPropertyDescriptor(reactObj,'name'));
+//
+// Object.defineProperty(reactObj,'name',{
+//     writable:false
+// })
+// reactObj.name='vbnmcxxxx'
+// console.log(Object.getOwnPropertyDescriptor(reactObj,'name'));
 
-Object.defineProperty(reactObj,'name',{
-    writable:false
+
+
+//setting enumerable
+Object.defineProperty(reactObj,'number',{
+    enumerable:false
 })
-reactObj.name='vbnmcxxxx'
-console.log(Object.getOwnPropertyDescriptor(reactObj,'name'));
+
+for (let [key,val] of Object.entries(reactObj)){
+    console.log(key,val)
+}
