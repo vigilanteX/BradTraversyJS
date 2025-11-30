be28fda839b062f89a12572196df341c48823aa7
vigilanteX <dixitaman8@gmail.com>
2025-11-30 11:52:30 +0530

    more on object literals

--- Section14/moreOnObjectLiterals.js
+++ Section14/moreOnObjectLiterals.js
@@ -1,25 +1,42 @@
-function Rectangle(name,width,height)
-{
-    this.name=name;
-    this.width=width;
-    this.height=height;
-    this.area=function()
-    {
-        return this.height*this.width;
+function Rectangle(name, width, height) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.area = function () {
+        return this.height * this.width;
     }
 }
-const rect1=new Rectangle('rectangle1',2,2);
-const rect2=new Rectangle('rectangle2',3,3);
-rect1.newprop=123;
-rect1.age=function()
-{
-    return 25
-}
-console.log(rect1);
-//console.log(rect1.age());
-delete rect1.newprop
-console.log(rect1);
 
+const rect1 = new Rectangle('rectangle1', 2, 2);
+const rect2 = new Rectangle('rectangle2', 3, 3);
+
+// //add prop and function
+rect1.newprop = 123;
+// rect1.age=function()
+// {
+//     return 25
+// }
+// console.log(rect1);
+// //console.log(rect1.age());
+//
+// //delete prop
+// delete rect1.newprop
+// console.log(rect1);
+//
+// //check object prop
+// console.log(rect1.hasOwnProperty('name'));
+//
+// //check keys
+// console.log(Object.keys(rect1))
+// //check values
+// console.log(Object.values(rect1))
+//
+// console.log(Object.entries(rect1))
+
+//iterate over KVP
+// for (let [key, value] of Object.entries(rect1)) {
+//     console.log(`Rectangle ${key} is ${value}`);
+// }
 
 
 
