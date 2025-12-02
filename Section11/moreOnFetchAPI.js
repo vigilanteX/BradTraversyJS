c50fcc9b4ee624845ab7cb12b66419347e22b1a3
vigilanteX <dixitaman8@gmail.com>
2025-12-02 22:51:33 +0530

    section17-error handling in fetch. Note-fetch() .catch works only if network error. For others throw error inside then catch will work

--- Section11/moreOnFetchAPI.js
+++ Section11/moreOnFetchAPI.js
@@ -1,7 +1,13 @@
-fetch('http://localhost:3000/checkpost',{
+fetch('http://localhost:3000/checkpost', {
     method: 'POST',
-    body: JSON.stringify({name:"Aman",age:26,height:181}),
-    headers: {'Brand':'SGB'}
+    body: JSON.stringify({name: "Aman", age: 26, height: 181}),
+    headers: {'Brand': 'SGB'}
 })
-.then(res => res.status)
-.then(data => console.log(data))
+    .then(res => {
+        console.log(res.status)
+        if (res.status === 500) {
+            throw new Error('Something went wrong')
+        }
+        return res
+    })
+    .then(data => console.log('data then 2')).catch(err => console.log('failed'));
